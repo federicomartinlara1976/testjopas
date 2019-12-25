92afc6e9ec01772d0e004e7ec5fcebcade8dda67
Federico Martín Lara <federicomartinlara1976@gmail.com>
2019-12-25 16:35:08 +0100

    Algebra

---
+++ src/main/java/net/bounceme/chronos/testjopas/controllers/AlgebraBean.java
@@ -0,0 +1,118 @@
+package net.bounceme.chronos.testjopas.controllers;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import net.bounceme.chronos.logger.Log;
+import net.bounceme.chronos.logger.LogFactory;
+import net.bounceme.chronos.testjopas.common.TestJopasConstantes;
+import net.bounceme.chronos.testjopas.common.TestJopasConstantes.Paths;
+import net.bounceme.chronos.testjopas.dto.AlgebraDTO;
+import net.bounceme.chronos.testjopas.exceptions.ServiceException;
+import net.bounceme.chronos.utils.jsf.controller.BaseBean;
+
+/**
+ * The Class SessionBean.
+ */
+@ManagedBean(name = AlgebraBean.NAME)
+@ViewScoped
+public class AlgebraBean extends BaseBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2350030970399677473L;
+
+	/** The Constant NAME. */
+	public static final String NAME = "algebraBean";
+
+	/** The logger. */
+	private Log logger;
+
+	/** The app bean. */
+	@ManagedProperty(value = "#{appBean}")
+	private AppBean appBean;
+
+	/** The app bean. */
+	@ManagedProperty(value = "#{sessionBean}")
+	private SessionBean sessionBean;
+
+	private AlgebraDTO algebraDTO;
+
+	@PostConstruct
+	public void initialize() {
+		try {
+			logger = LogFactory.getInstance().getLogger(AlgebraBean.class, "LOG4J");
+
+			TestJopasConstantes.Paths paths = (TestJopasConstantes.Paths) this.getJsfHelper()
+					.getSessionAttribute("path");
+
+			appBean.getCalcService().clearEnvironment();
+			appBean.getCalcService().resetPath();
+			appBean.getCalcService().addPath(Paths.funciones.value());
+			appBean.getCalcService().addPath(paths.value());
+
+			reset();
+		} catch (ServiceException e) {
+			logger.error("ERROR:", e);
+			this.addErrorMessage(e);
+		}
+	}
+
+	public void calcular() {
+		
+	}
+
+	public void reset() {
+		algebraDTO = new AlgebraDTO();
+	}
+	
+	public void cambiarCoeficientes() {
+		Integer n = algebraDTO.getNumeroCoeficientes();
+		algebraDTO.setMatrizCoeficientes(new BigDecimal[n][n]);
+		algebraDTO.setTerminos(new BigDecimal[n]);
+	}
+
+	/**
+	 * @return the appBean
+	 */
+	public AppBean getAppBean() {
+		return appBean;
+	}
+
+	/**
+	 * @param appBean the appBean to set
+	 */
+	public void setAppBean(AppBean appBean) {
+		this.appBean = appBean;
+	}
+
+	public SessionBean getSessionBean() {
+		return sessionBean;
+	}
+
+	public void setSessionBean(SessionBean sessionBean) {
+		this.sessionBean = sessionBean;
+	}
+
+	/**
+	 * @return the algebraDTO
+	 */
+	public AlgebraDTO getAlgebraDTO() {
+		return algebraDTO;
+	}
+
+	/**
+	 * @param algebraDTO the algebraDTO to set
+	 */
+	public void setAlgebraDTO(AlgebraDTO algebraDTO) {
+		this.algebraDTO = algebraDTO;
+	}
+
+	
+}
---
+++ src/main/java/net/bounceme/chronos/testjopas/dto/AlgebraDTO.java
@@ -0,0 +1,74 @@
+package net.bounceme.chronos.testjopas.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class AlgebraDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4085451201013746161L;
+	
+	private Integer numeroCoeficientes;
+	
+	private BigDecimal[][] matrizCoeficientes;
+	
+	private BigDecimal[] terminos;
+
+	public AlgebraDTO() {
+		numeroCoeficientes = 2;
+		matrizCoeficientes = new BigDecimal[numeroCoeficientes][numeroCoeficientes];
+		terminos = new BigDecimal[numeroCoeficientes];
+		
+		for (int i=0;i<numeroCoeficientes;i++) {
+			for (int j=0;j<numeroCoeficientes;j++) {
+				matrizCoeficientes[i][j] = BigDecimal.ZERO;
+			}
+			
+			terminos[i] = BigDecimal.ZERO;
+		}
+	}
+
+	/**
+	 * @return the numeroCoeficientes
+	 */
+	public Integer getNumeroCoeficientes() {
+		return numeroCoeficientes;
+	}
+
+	/**
+	 * @param numeroCoeficientes the numeroCoeficientes to set
+	 */
+	public void setNumeroCoeficientes(Integer numeroCoeficientes) {
+		this.numeroCoeficientes = numeroCoeficientes;
+	}
+
+	/**
+	 * @return the matrizCoeficientes
+	 */
+	public BigDecimal[][] getMatrizCoeficientes() {
+		return matrizCoeficientes;
+	}
+
+	/**
+	 * @param matrizCoeficientes the matrizCoeficientes to set
+	 */
+	public void setMatrizCoeficientes(BigDecimal[][] matrizCoeficientes) {
+		this.matrizCoeficientes = matrizCoeficientes;
+	}
+
+	/**
+	 * @return the terminos
+	 */
+	public BigDecimal[] getTerminos() {
+		return terminos;
+	}
+
+	/**
+	 * @param terminos the terminos to set
+	 */
+	public void setTerminos(BigDecimal[] terminos) {
+		this.terminos = terminos;
+	}
+}
