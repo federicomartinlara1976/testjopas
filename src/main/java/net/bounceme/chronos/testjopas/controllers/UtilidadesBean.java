537f78b0a9e7b0a774deb7f3ec8cdb50f6e48476
Federico Martín Lara <federicomartinlara1976@gmail.com>
2020-02-06 23:29:30 +0100

    Bean de utilidades

---
+++ src/main/java/net/bounceme/chronos/testjopas/controllers/UtilidadesBean.java
@@ -0,0 +1,85 @@
+package net.bounceme.chronos.testjopas.controllers;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.bounceme.chronos.logger.Log;
+import net.bounceme.chronos.logger.LogFactory;
+import net.bounceme.chronos.testjopas.common.TestJopasConstantes;
+import net.bounceme.chronos.testjopas.common.TestJopasConstantes.Paths;
+import net.bounceme.chronos.testjopas.controllers.converters.FileSelectItemConverter;
+import net.bounceme.chronos.testjopas.dto.AlgoritmoDtwDTO;
+import net.bounceme.chronos.testjopas.exceptions.ServiceException;
+import net.bounceme.chronos.testjopas.services.AlgoritmoDtwService;
+import net.bounceme.chronos.testjopas.services.FilesService;
+import net.bounceme.chronos.utils.calc.dto.MatrixDTO;
+import net.bounceme.chronos.utils.jsf.controller.BaseBean;
+import net.bounceme.chronos.velazquez.services.ComparadorFirmas;
+
+/**
+ * @author Federico Martín Lara
+ *
+ */
+@ManagedBean(name = UtilidadesBean.NAME)
+@ViewScoped
+public class UtilidadesBean extends BaseBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2350030970399677473L;
+
+	/** The Constant NAME. */
+	public static final String NAME = "utilidadesBean";
+
+	/** The logger. */
+	private transient Log logger;
+
+	/** The app bean. */
+	@ManagedProperty(value = "#{appBean}")
+	private AppBean appBean;
+
+	/** The app bean. */
+	@ManagedProperty(value = "#{sessionBean}")
+	private SessionBean sessionBean;
+	
+	@Autowired
+	private transient FilesService filesService;
+
+	@PostConstruct
+	public void initialize() {
+		logger = LogFactory.getInstance().getLogger(UtilidadesBean.class, "LOG4J");
+	}
+
+	/**
+	 * @return the appBean
+	 */
+	public AppBean getAppBean() {
+		return appBean;
+	}
+
+	/**
+	 * @param appBean the appBean to set
+	 */
+	public void setAppBean(AppBean appBean) {
+		this.appBean = appBean;
+	}
+
+	public SessionBean getSessionBean() {
+		return sessionBean;
+	}
+
+	public void setSessionBean(SessionBean sessionBean) {
+		this.sessionBean = sessionBean;
+	}
+}
