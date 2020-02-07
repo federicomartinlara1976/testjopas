c08c0a4d453a1bb8607ca43a585854d02466cb60
Federico Martín Lara <federicomartinlara1976@gmail.com>
2020-02-07 08:47:28 +0100

    Update UtilidadesBean.java

--- src/main/java/net/bounceme/chronos/testjopas/controllers/UtilidadesBean.java
+++ src/main/java/net/bounceme/chronos/testjopas/controllers/UtilidadesBean.java
@@ -1,30 +1,20 @@
 package net.bounceme.chronos.testjopas.controllers;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
-import javax.faces.model.SelectItem;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import net.bounceme.chronos.logger.Log;
 import net.bounceme.chronos.logger.LogFactory;
-import net.bounceme.chronos.testjopas.common.TestJopasConstantes;
-import net.bounceme.chronos.testjopas.common.TestJopasConstantes.Paths;
-import net.bounceme.chronos.testjopas.controllers.converters.FileSelectItemConverter;
-import net.bounceme.chronos.testjopas.dto.AlgoritmoDtwDTO;
 import net.bounceme.chronos.testjopas.exceptions.ServiceException;
-import net.bounceme.chronos.testjopas.services.AlgoritmoDtwService;
 import net.bounceme.chronos.testjopas.services.FilesService;
-import net.bounceme.chronos.utils.calc.dto.MatrixDTO;
 import net.bounceme.chronos.utils.jsf.controller.BaseBean;
-import net.bounceme.chronos.velazquez.services.ComparadorFirmas;
 
 /**
  * @author Federico Martín Lara
