cd94417907764d385122e66d3108a1e803dacf78
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-14 11:47:58 +0200

    Update converters

--- src/main/java/net/bounceme/chronos/testjopas/services/JavaOctaveService.java
+++ src/main/java/net/bounceme/chronos/testjopas/services/JavaOctaveService.java
@@ -1,7 +1,7 @@
 package net.bounceme.chronos.testjopas.services;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.math.RoundingMode;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -13,11 +13,9 @@ import dk.ange.octave.type.OctaveDouble;
 import dk.ange.octave.type.OctaveString;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.bounceme.chronos.utils.calc.converters.Converter;
 import net.bounceme.chronos.utils.calc.converters.OctaveDoubleToArray;
-import net.bounceme.chronos.utils.calc.converters.OctaveDoubleToBigDecimal;
-import net.bounceme.chronos.utils.calc.converters.OctaveDoubleToInteger;
 import net.bounceme.chronos.utils.calc.converters.OctaveDoubleToMatrix;
-import net.bounceme.chronos.utils.calc.converters.OctaveStringToString;
 import net.bounceme.chronos.utils.calc.dto.MatrixDTO;
 import net.bounceme.chronos.utils.calc.dto.VectorDTO;
 
@@ -27,24 +25,15 @@ public class JavaOctaveService implements CalcService {
 
 	private OctaveEngine octave;
 
-	private OctaveDoubleToBigDecimal doubleToBigDecimal;
-
-	private OctaveDoubleToInteger octaveIntToInteger;
-
 	private OctaveDoubleToArray octaveDoubleToArray;
 
 	private OctaveDoubleToMatrix octaveDoubleToMatrix;
 
-	private OctaveStringToString octaveStringToString;
-
 	public JavaOctaveService() {
 		super();
 		octave = new OctaveEngineFactory().getScriptEngine();
-		doubleToBigDecimal = new OctaveDoubleToBigDecimal();
-		octaveIntToInteger = new OctaveDoubleToInteger();
 		octaveDoubleToArray = new OctaveDoubleToArray();
 		octaveDoubleToMatrix = new OctaveDoubleToMatrix();
-		octaveStringToString = new OctaveStringToString();
 	}
 
 	@SneakyThrows
@@ -135,36 +124,37 @@ public class JavaOctaveService implements CalcService {
 
 	@Override
 	public List<String> getVars() {
-		return new ArrayList<>(OctaveUtils.listVars(octave));
+		return OctaveUtils.listVars(octave).stream().toList();
 	}
 
 	@Override
 	public BigDecimal getScalar(String name) {
-		OctaveDouble value = octave.get(OctaveDouble.class, name);
-		return doubleToBigDecimal.convert(value);
+		Converter<OctaveDouble, BigDecimal> converter = s -> BigDecimal.valueOf(s.getData()[0])
+                .setScale(0, RoundingMode.HALF_UP);
+		return converter.apply(octave.get(OctaveDouble.class, name));
 	}
 
 	@Override
 	public Integer getIntScalar(String name) {
-		OctaveDouble value = octave.get(OctaveDouble.class, name);
-		return octaveIntToInteger.convert(value);
+		Converter<OctaveDouble, Integer> converter = s -> BigDecimal.valueOf(s.getData()[0])
+                .setScale(0, RoundingMode.HALF_UP) // Redondeo estándar
+                .intValueExact();
+		return converter.apply(octave.get(OctaveDouble.class, name));
 	}
 
 	@Override
 	public BigDecimal[] getArray(String name) {
-		OctaveDouble value = octave.get(OctaveDouble.class, name);
-		return octaveDoubleToArray.convert(value);
+		return octaveDoubleToArray.apply(octave.get(OctaveDouble.class, name));
 	}
 
 	@Override
 	public BigDecimal[][] getMatrix(String name) {
-		OctaveDouble value = octave.get(OctaveDouble.class, name);
-		return octaveDoubleToMatrix.assemble(value);
+		return octaveDoubleToMatrix.apply(octave.get(OctaveDouble.class, name));
 	}
 
 	@Override
 	public String getString(String name) {
-		OctaveString value = octave.get(OctaveString.class, name);
-		return octaveStringToString.convert(value);
+		Converter<OctaveString, String> converter = s -> s.getString();
+		return converter.apply(octave.get(OctaveString.class, name));
 	}
 }
